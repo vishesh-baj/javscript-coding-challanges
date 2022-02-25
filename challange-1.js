b4f76dd1670d1cbb7627c9f748adaa775811df05
vishesh-baj <vishesh.bajpayee@icloud.com>
2022-02-25 17:20:37 -0600

    added javascript oding chalanges repo

---
+++ challange-1.js
@@ -0,0 +1,11 @@
+// Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
+// Your tasks:
+// 1. StoreMark'sandJohn'smassandheightinvariables
+// 2. CalculateboththeirBMIsusingtheformula(youcanevenimplementboth
+// versions)
+// 3. CreateaBooleanvariable'markHigherBMI'containinginformationabout
+// whether Mark has a higher BMI than John.
+// Test data:
+// § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+// § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
